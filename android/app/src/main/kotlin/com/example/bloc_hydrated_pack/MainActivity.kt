05d5a4ff34aa8586ad1cbe7f1493378a38fa7f14
Romu4h <roman.shevchuk.job@gmail.com>
2023-03-22 10:09:16 +0200

    final variant

---
+++ android/app/src/main/kotlin/com/example/bloc_hydrated_pack/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_hydrated_pack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/logic/cubit/internet_state.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../constants/enums.dart';
+
+@immutable
+abstract class InternetState {}
+
+class InternetLoading extends InternetState {}
+
+class InternetConnected extends InternetState {
+  final ConnectionType connectionType;
+
+  InternetConnected({required this.connectionType});
+}
+
+class InternetDisconnected extends InternetState {}
